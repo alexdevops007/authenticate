892733c8c766acb5f882a5deeeeb9467dacac8fa
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2023-09-24 03:02:20 +0200

    Update User.js

--- models/User.js
+++ models/User.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ['user', 'admin'], // Rôles utilisateur (ajustez selon vos besoins)
+    default: 'user',
+  },
 });
 
 module.exports = mongoose.model('User', userSchema);

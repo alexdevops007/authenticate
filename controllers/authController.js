2b8e1da41bb2441a5fb07f8db93271acf8948788
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2023-09-24 02:54:44 +0200

    Update authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,49 @@
+const User = require('../models/User');
+const argon2 = require('argon2');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
+
+const register = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const hashedPassword = await argon2.hash(password);
+    const user = new User({ username, password: hashedPassword });
+    await user.save();
+    res.status(201).json({ message: 'Utilisateur enregistré avec succès.' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de l'enregistrement de l'utilisateur.' });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Nom d'utilisateur ou mot de passe incorrect.' });
+    }
+
+    const validPassword = await argon2.verify(user.password, password);
+
+    if (!validPassword) {
+      return res.status(401).json({ error: 'Nom d'utilisateur ou mot de passe incorrect.' });
+    }
+
+    const accessToken = jwt.sign({ userId: user._id }, config.accessTokenSecret, {
+      expiresIn: '15m',
+    });
+
+    const refreshToken = jwt.sign({ userId: user._id }, config.refreshTokenSecret, {
+      expiresIn: '7d',
+    });
+
+    res.json({ accessToken, refreshToken });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Erreur lors de la connexion.' });
+  }
+};
+
+module.exports = { register, login };

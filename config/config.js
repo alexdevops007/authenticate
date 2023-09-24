36f145228237228fe13276a7b8e4b50373d4892d
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2023-09-24 02:53:54 +0200

    Update config.js

---
+++ config/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  dbURL: 'mongodb+srv://xandermobutu:123auth123@auth.ryenpu6.mongodb.net/?retryWrites=true&w=majority',
+  secretKey: 'votreSecretKey',
+  accessTokenSecret: 'votreAccessTokenSecret',
+  refreshTokenSecret: 'votreRefreshTokenSecret',
+};

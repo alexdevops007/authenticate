cef0ca00bfef0e63c5552d904b42f29b3c03ad79
Alex Simisi Nta <117230604+alexdevops007@users.noreply.github.com>
2023-09-24 03:00:12 +0200

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -6,9 +6,14 @@ const authMiddleware = require('../middleware/authMiddleware');
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// Route protégée
+// Route protégée avec authentification
 router.get('/resource-protegee', authMiddleware.authenticateToken, (req, res) => {
   res.json({ message: 'Ceci est une ressource protégée.' });
+});
+
+// Route protégée avec authentification et autorisation
+router.get('/resource-admin', authMiddleware.authenticateToken, authMiddleware.authorizeUser, (req, res) => {
+  res.json({ message: 'Ceci est une ressource accessible uniquement aux administrateurs.' });
 });
 
 module.exports = router;
